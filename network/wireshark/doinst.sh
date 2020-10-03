31645bf2fd7376d4aa80341d17616a4e2554d1f1
Andrzej Telszewski <atelszewski@gmail.com>
2020-10-03 13:55:01 +0200

    network/wireshark: Allow users in wireshark group to use dumpcap

--- network/wireshark/doinst.sh
+++ network/wireshark/doinst.sh
@@ -7,3 +7,9 @@ if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
     /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
   fi
 fi
+
+if [ -e usr/bin/dumpcap ] && grep -q ^wireshark: etc/group; then
+  /bin/chgrp wireshark usr/bin/dumpcap
+  /bin/chmod 750 usr/bin/dumpcap
+  /sbin/setcap cap_net_raw,cap_net_admin=eip usr/bin/dumpcap
+fi
