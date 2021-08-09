fe571dc7e1d5f0c10bd9f7c42e98e8d32b05b8d9
Andrzej Telszewski <andrzej@telszewski.com>
2021-08-09 13:31:19 +0200

    system/kvdo: Add (Kernel Modules for Virtual Data Optimizer)

---
+++ system/kvdo/put-source.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+set -u
+set -o pipefail
+
+source $(pwd)/*.info
+
+sftpuser=atelszewski
+sftphost=web.sourceforge.net
+sftpopt=(-o PasswordAuthentication=no)
+destdir=/home/frs/project/slackbuildsdirectlinks/${PRGNAM}
+
+sftp "${sftpopt[@]}" ${sftpuser}@${sftphost} << EOF
+    mkdir ${destdir}
+    cd ${destdir}
+    put ${PRGNAM}-${VERSION}.tar.gz
+    put ${PRGNAM}-${VERSION}.tar.gz.md5
+EOF
