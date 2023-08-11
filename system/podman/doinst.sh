0f6154cef080584b519502f4d97087dbcf5b2fab
Andrzej Telszewski <andrzej@telszewski.com>
2023-08-11 10:35:16 +0200

    system/podman: !skip,no-merge,fixme: Update to version 4.7.2

--- system/podman/doinst.sh
+++ system/podman/doinst.sh
@@ -11,6 +11,14 @@ config() {
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
+<<<<<<< HEAD
 config etc/containers/registries.conf.new
 config etc/containers/mounts.conf.new
+=======
+config etc/containers/containers.conf.new
+config etc/containers/registries.conf.new
+config etc/containers/mounts.conf.new
+## **FIXME** Remove if /usr/share/containers/seccomp.json is installed by default.
+#config etc/containers/seccomp.json.new
+>>>>>>> b70ec0e130 (!no-merge,fixme: system/podman: Update to version 4.6.0)
 config etc/containers/policy.json.new
